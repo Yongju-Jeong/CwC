5f8296758cb8c7b40f438f751c64ed212ce1b111
Yongju Jeong <yongjuaa@gmail.com>
2022-10-13 03:06:45 +0900

    Solve problem in week2

---
+++ NeuronA/2주차/주식가격.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+def solution(prices: list) -> list:
+    prices_deq = deque(prices)
+    answer = []
+    
+    while prices_deq:
+        cmp_value = prices_deq.popleft()
+        
+        low_idxs = next((v for v in prices_deq if cmp_value > v), None)
+        if low_idxs is not None:
+            answer.append(prices_deq.index(low_idxs)+1)
+        else:
+            answer.append(len(prices_deq))
+    
+    return answer
